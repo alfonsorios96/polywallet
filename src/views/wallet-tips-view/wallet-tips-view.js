e566b1e065fefb0c0cc5c3aaaa15594aa9e821c6
Luis Zepeda <lzmonter@gmail.com>
2017-11-18 18:08:54 -0600

    feat: add new view(wallet tips)

---
+++ src/views/wallet-tips-view/wallet-tips-view.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    /**
+     * @customElement
+     * @polymer
+     */
+class WalletTipsView extends ReduxMixin(Polymer.Element) {
+
+        static get is() {
+            return 'wallet-tips-view';
+        }
+
+        connectedCallback() {
+            super.connectedCallback();
+            const state = this.getState();
+            console.log(state);
+        }
+
+        static get properties() {
+            return {
+                prop1: {
+                    type: String,
+                    statePath: 'title',
+                },
+            };
+        }
+    }
+    window.customElements.define(WalletTipsView.is, WalletTipsView);
+})();
